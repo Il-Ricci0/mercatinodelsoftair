413977c19a0e007e4640cd66812b16ccf98da225
Riccardo Roman <98636234+Il-Ricci0@users.noreply.github.com>
2023-02-06 09:55:36 +0100

    added subcategories

--- templates/admin_template.js
+++ templates/admin_template.js
@@ -7,7 +7,7 @@ document.getElementById("header").innerHTML =
     + '<link rel="stylesheet" href="../../vendor/fontawesome/css/all.css"></link>'
     + '<nav class="navbar bg-danger p-2">'
     + '<a class="navbar-brand" href="index.html">'
-    + '<img src="../../resurces/logo.svg" width="150">'
+    + '<img src="../../resurces/logo.svg" width="100">'
     + '</a>'
     + '<form class="form-inline d-flex">'
     + '<input class="form-control rounded-end-0" type="search" placeholder="cerca utente">'
