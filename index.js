09382e793480c809cec510eb9e424ae86c8c1aca
Riccardo Roman <98636234+Il-Ricci0@users.noreply.github.com>
2023-04-09 14:08:52 +0200

    +search function working perfect in coop with category filter

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 function expandOrCollapseCategory() {
     let urlParams = new URLSearchParams(window.location.search);
     let id = urlParams.get('id');
-    let subcategories = document.querySelectorAll(".subcategory");
+    let subcategories = document.querySelectorAll('.subcategory');
     subcategories.forEach(subcategory => {
-        subcategory.setAttribute("hidden", "");
+        subcategory.setAttribute('hidden', '');
     });
     subcategories = document.getElementsByName(id);
     subcategories.forEach(subcategory => {
-        if (subcategory.hasAttribute("hidden"))
-            subcategory.removeAttribute("hidden");
+        if (subcategory.hasAttribute('hidden'))
+            subcategory.removeAttribute('hidden');
     });
     try {
 
         let macrocategory = document.getElementById(id).parentElement.getAttribute('name');
         subcategories = document.getElementsByName(macrocategory);
         subcategories.forEach(subcategory => {
-            if (subcategory.hasAttribute("hidden"))
-                subcategory.removeAttribute("hidden");
+            if (subcategory.hasAttribute('hidden'))
+                subcategory.removeAttribute('hidden');
         });
         let selected = document.getElementById(id);
         selected.classList.add('bg-primary');
@@ -29,39 +29,48 @@ function filter(id, category) {
     let urlParams = new URLSearchParams(window.location.search);
     let queryId = urlParams.get('id');
     let search = urlParams.get('search');
-    if (queryId == id) {
-        window.location.href = "/mercatinodelsoftair/index.php";
+    if (queryId == id && search == null) {
+        window.location.href = '/mercatinodelsoftair/index.php';
+    }
+    else if (queryId == id && search != null) {
+        window.location.href = '/mercatinodelsoftair/index.php?search=' + search;
+    }
+    else if (search != null) {
+        window.location.href = '/mercatinodelsoftair/index.php?id=' + id + '&' + 'category=' + category + '&search=' + search;
     } else {
-        window.location.href = "/mercatinodelsoftair/index.php?id=" + id + "&" + "category=" + category + "&search=" + search;
+        window.location.href = '/mercatinodelsoftair/index.php?id=' + id + '&' + 'category=' + category;
     }
 }
 
 function expandOrCollapseUserMenu() {
-    menu = document.getElementById("user-menu");
-    if (menu.hasAttribute("hidden")) {
-        menu.removeAttribute("hidden");
+    menu = document.getElementById('user-menu');
+    if (menu.hasAttribute('hidden')) {
+        menu.removeAttribute('hidden');
     }
     else {
-        menu.setAttribute("hidden", "");
+        menu.setAttribute('hidden', '');
     }
 }
 
 function updateSearchPlaceholder() {
     let urlParams = new URLSearchParams(window.location.search);
     let search = urlParams.get('search');
-    document.getElementById("search").value = search;
+    document.getElementById('search').value = search;
 }
 
 function search() {
-    let search = document.getElementById("search").value;
+    let search = document.getElementById('search').value;
     let urlParams = new URLSearchParams(window.location.search);
     let id = urlParams.get('id');
     let category = urlParams.get('category');
-    if(id != null && category != null){
-        window.location.href = "/mercatinodelsoftair/index.php?id=" + id + "&" + "category=" + category + "&search=" + search;
+    if (id != null && category != null && search != null) {
+        window.location.href = '/mercatinodelsoftair/index.php?id=' + id + '&' + 'category=' + category + '&search=' + search;
     }
-    else{
-        window.location.href = "/mercatinodelsoftair/index.php?search=" + search;
+    else if (search != null) {
+        window.location.href = '/mercatinodelsoftair/index.php?search=' + search;
+    }
+    else {
+        window.location.href = '/mercatinodelsoftair/index.php';
     }
 }
 
